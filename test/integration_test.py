f111db9177df06ddbe779682a491b3866d48c556
Andrew Neitsch <andrew@neitsch.ca>
2021-03-01 16:57:35 -0700

    Add ssh server to test setup

--- test/integration_test.py
+++ test/integration_test.py
@@ -2,6 +2,7 @@ import secrets
 from pathlib import Path
 from shutil import which
 from subprocess import check_call, check_output
+from textwrap import dedent
 
 import pytest
 
@@ -11,11 +12,18 @@ LFS_PUSH_URL = "ssh://localhost:6422/"
 
 
 @pytest.fixture
-def tmp_path2(tmpdir_factory):
-    "For when you need more than one tmp_dir in a test"
-    # see https://github.com/pytest-dev/pytest/issues/2703
-    # “Multiple use of fixture in a single test”
-    return tmpdir_factory.mktemp("tmp-path2")
+def origin_dir(tmpdir_factory):
+    return Path(tmpdir_factory.mktemp("origin"))
+
+
+@pytest.fixture
+def clone_dir(tmpdir_factory):
+    return Path(tmpdir_factory.mktemp("clone"))
+
+
+@pytest.fixture
+def new_clone_dir(tmpdir_factory):
+    return Path(tmpdir_factory.mktemp("new-clone"))
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -39,7 +47,15 @@ def create_git_lfs_repo(path, foo_contents="foo"):
     check_call(["git", "init"], cwd=path)
 
     check_call(["git", "lfs", "install"], cwd=path)
-    (path / ".lfsconfig").write_text(f"[lfs]\n url = {LFS_PULL_URL}")
+    (path / ".lfsconfig").write_text(
+        dedent(
+            f"""\
+                [lfs]
+                 url = {LFS_PULL_URL}
+                 pushUrl = {LFS_PUSH_URL}
+            """
+        )
+    )
     check_call(["git", "add", ".lfsconfig"], cwd=path)
 
     (path / "foo").write_text(foo_contents)
@@ -50,33 +66,40 @@ def create_git_lfs_repo(path, foo_contents="foo"):
     check_call(["git", "commit", "-m", "add foo"], cwd=path)
 
 
-def test_authorized_user_can_commit(tmp_path, tmp_path2):
-    assert tmp_path != tmp_path2
+def create_and_push(clone_dir, origin_dir):
+    assert clone_dir != origin_dir
 
     foo_contents = "big binary file " + secrets.token_hex(20)
 
-    origin = tmp_path
-    check_call(["git", "init", "--bare"], cwd=origin)
+    check_call(["git", "init", "--bare"], cwd=origin_dir)
 
-    clone = Path(tmp_path2)
-    create_git_lfs_repo(clone, foo_contents=foo_contents)
+    create_git_lfs_repo(clone_dir, foo_contents=foo_contents)
 
-    check_call(["git", "remote", "add", "origin", origin], cwd=clone)
-    check_call(["git", "push"], cwd=clone)
+    check_call(["git", "remote", "add", "origin", origin_dir], cwd=clone_dir)
+    check_call(["git", "push"], cwd=clone_dir)
+
+    return foo_contents
+
+
+def test_authorized_user_can_commit(clone_dir, origin_dir):
+    foo_contents = create_and_push(clone_dir, origin_dir)
 
     # The upstream repo should contain a pointer to the contents, not the
     # contents themselves.
-    foo_blob = check_output(["git", "cat-file", "blob", "HEAD:foo"], cwd=origin)
+    foo_blob = check_output(["git", "cat-file", "blob", "HEAD:foo"], cwd=origin_dir)
 
     assert foo_contents.encode("UTF-8") not in foo_blob
     assert b"version https://git-lfs.github.com/spec" in foo_blob
 
 
-@pytest.mark.skip()
-def test_anon_users_cannot_push():
-    pass
+def test_anon_users_cannot_push(clone_dir, new_clone_dir):
+    with pytest.raises(Exception, match="unauthorized"):
+        create_and_push(clone_dir, new_clone_dir)
 
 
-@pytest.mark.skip
-def test_anon_users_can_pull():
-    pass
+def test_anon_users_can_pull(clone_dir, origin_dir, new_clone_dir):
+    foo_contents = create_and_push(clone_dir, origin_dir)
+
+    check_call(["git", "clone", origin_dir, "new"], cwd=new_clone_dir)
+
+    assert foo_contents in (new_clone_dir / "new" / "foo").read_text()
